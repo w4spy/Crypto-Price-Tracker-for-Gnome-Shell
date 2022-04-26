aa43925e558865aa4588cd7b7e7db6a4db6430dd
w4spy <40596946+w4spy@users.noreply.github.com>
2022-04-26 16:00:35 +0100

    added

--- api/data.js
+++ api/data.js
@@ -32,17 +32,21 @@ var getPrice = async function (name, vol) {
 };
 
 async function _getPriceFromBinance(name, vol) {
-  const url = 'https://api.binance.com/api/v3/ticker/price?symbol=';
+  const url = 'https://api.binance.com/api/v3/ticker/24hr?symbol=';
   const res = await request.get(url + name + vol);
+  const me_res = await request.get('https://api.binance.com/api/v3/klines?symbol='+name+vol+'&interval=1d&limit=1');
 
   const jsonRes = JSON.parse(res.body);
   if (jsonRes.code) return jsonRes.msg.slice(0, 30) + '...';
-
-  let price = +jsonRes.price;
-
-  let maximumFractionDigits = 0;
-  const dig_count = price.toFixed().length;
-  if (5 - dig_count > 0) maximumFractionDigits = 5 - dig_count;
+  let dailychange = +jsonRes.priceChangePercent.slice(0,4) + '%d';
+  let candleopen = JSON.parse(me_res.body)[0][1];
+  let me_price = jsonRes.lastPrice;
+  let change = (((me_price - candleopen)/candleopen) * 100).toString().slice(0,4);
+  let price = +jsonRes.lastPrice.slice(0,6) + '  ' + change + '  ' + dailychange;
+  //2 lines under commented by me
+  let maximumFractionDigits = 0; 
+  //const dig_count = price.toFixed().length;
+  //if (5 - dig_count > 0) maximumFractionDigits = 5 - dig_count;
 
   return price.toLocaleString(undefined, { maximumFractionDigits });
 }
